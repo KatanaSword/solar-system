10f10c4efb6f4c9a1f85c836898d4d8f5cf49e86
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2023-12-27 12:52:46 +0530

    refactor - update app.js

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ function greeting() {
     log("💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮");
     log(chalk.magenta.bold.italic("Welcome to the world of stars and planets"));
     log("💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮💮");
+    userName()
 };
 greeting();
 
 function userName() {
     userInput = readlineSync.question(chalk.cyan.bold("Please enter your name: "));
-    if(userInput === "") {
+    if(!userInput || !isNaN(userInput)) {
         log(chalk.red.bold("Ooops! error occured, Please enter your name"));
         userName()
     } else {
         log("🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻")
         log(`${chalk.green.bold.italic(userInput)} ${chalk.green.bold("Welcome to the solar system")}`);
         log("🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻🌻")
-        exitOrContinueFunc();
+        exitOrContinueFunc()
     };
 };
-userName();
 
 function solarSystem() {
     const starsAndPlanets = readlineSync.question(chalk.blue.bold("Please enter the name of star and planet: "));
@@ -32,7 +32,7 @@ function solarSystem() {
 
     for(let i = 0; i < solarSystemPlanets.length; i++) {
         if(starsAndPlanets === solarSystemPlanets[i].name || starsAndPlanets === solarSystemPlanets[i].name.toLowerCase()) {
-            fetchPlanetInformation(solarSystemPlanets[i]);
+            fetchPlanetInformation(solarSystemPlanets[i])
             found = true;
             break;
         };
@@ -41,7 +41,7 @@ function solarSystem() {
     if(!found) {
         for(let i = 0; i < stars.length; i++) {
             if(starsAndPlanets === stars[i].name || starsAndPlanets === stars[i].name.toLowerCase()) {
-                fetchStarInformation(stars[i]);
+                fetchStarInformation(stars[i])
                 found = true;
                 break;
             };
@@ -50,50 +50,50 @@ function solarSystem() {
 
     if(!found) {
         log(chalk.red.bold("Ooops! error occured, Please enter the name of star and planet"));
-        solarSystem();
+        solarSystem()
     };
-    exitOrContinueFunc();
+    exitOrContinueFunc()
 };
 
 function fetchPlanetInformation(planet) {
-    log(`${chalk.cyan.bold.underline("Planet Information")}${chalk.cyan.bold(":")}`)
-    log(`${chalk.cyan.bold("Name:")} ${chalk.white.bold(planet.name)}`)
-    log(`${chalk.cyan.bold("Type:")} ${chalk.white.bold(planet.type)}`)
-    log(`${chalk.cyan.bold("Mass:")} ${chalk.white.bold(planet.mass)}`)
-    log(`${chalk.cyan.bold("Diameter:")} ${chalk.white.bold(planet.diameter)}`)
-    log(`${chalk.cyan.bold("Distance from sun:")} ${chalk.white.bold(planet.distanceFromSun)}`)
-    log(`${chalk.cyan.bold("Distance from earth:")} ${chalk.white.bold(planet.distanceFromEarth)}`)
-    log(`${chalk.cyan.bold("Orbital period:")} ${chalk.white.bold(planet.orbitalPeriod)}`)
-    log(`${chalk.cyan.bold("Day length:")} ${chalk.white.bold(planet.dayLength)}`)
-    log(`${chalk.cyan.bold("Composition:")} ${chalk.white.bold(planet.composition)}`)
-    log(`${chalk.cyan.bold("Atmosphere:")} ${chalk.white.bold(planet.atmosphere)}`)
-    log(`${chalk.cyan.bold("Temperature:")} ${chalk.white.bold(planet.temperature)}`)
-    log(`${chalk.cyan.bold("Moons:")} ${chalk.white.bold(planet.moons)}`)
+    log(`${chalk.cyan.bold.underline("Planet Information")}${chalk.cyan.bold(":")}`);
+    log(`${chalk.cyan.bold("Name:")} ${chalk.white.bold(planet.name)}`);
+    log(`${chalk.cyan.bold("Type:")} ${chalk.white.bold(planet.type)}`);
+    log(`${chalk.cyan.bold("Mass:")} ${chalk.white.bold(planet.mass)}`);
+    log(`${chalk.cyan.bold("Diameter:")} ${chalk.white.bold(planet.diameter)}`);
+    log(`${chalk.cyan.bold("Distance from sun:")} ${chalk.white.bold(planet.distanceFromSun)}`);
+    log(`${chalk.cyan.bold("Distance from earth:")} ${chalk.white.bold(planet.distanceFromEarth)}`);
+    log(`${chalk.cyan.bold("Orbital period:")} ${chalk.white.bold(planet.orbitalPeriod)}`);
+    log(`${chalk.cyan.bold("Day length:")} ${chalk.white.bold(planet.dayLength)}`);
+    log(`${chalk.cyan.bold("Composition:")} ${chalk.white.bold(planet.composition)}`);
+    log(`${chalk.cyan.bold("Atmosphere:")} ${chalk.white.bold(planet.atmosphere)}`);
+    log(`${chalk.cyan.bold("Temperature:")} ${chalk.white.bold(planet.temperature)}`);
+    log(`${chalk.cyan.bold("Moons:")} ${chalk.white.bold(planet.moons)}`);
 };
 
 function fetchStarInformation(star) {
-    log(`${chalk.cyan.bold.underline("Star Information")}${chalk.cyan.bold(":")}`)
-    log(`${chalk.cyan.bold("Name:")} ${chalk.white.bold(star.name)}`)
-    log(`${chalk.cyan.bold("Type:")} ${chalk.white.bold(star.type)}`)
-    log(`${chalk.cyan.bold("Mass:")} ${chalk.white.bold(star.mass)}`)
-    log(`${chalk.cyan.bold("Diameter:")} ${chalk.white.bold(star.diameter)}`)
-    log(`${chalk.cyan.bold("Distance from earth:")} ${chalk.white.bold(star.distanceFromEarth)}`)
-    log(`${chalk.cyan.bold("Temperature:")} ${chalk.white.bold(star.temperature)}`)
-    log(`${chalk.cyan.bold("Composition:")} ${chalk.white.bold(star.composition)}`)
-    log(`${chalk.cyan.bold("Atmosphere:")} ${chalk.white.bold(star.atmosphere)}`)
-    log(`${chalk.cyan.bold("Fusion:")} ${chalk.white.bold(star.fusion)}`)
-    log(`${chalk.cyan.bold("planet:")} ${chalk.white.bold(star.planet)}`)
+    log(`${chalk.cyan.bold.underline("Star Information")}${chalk.cyan.bold(":")}`);
+    log(`${chalk.cyan.bold("Name:")} ${chalk.white.bold(star.name)}`);
+    log(`${chalk.cyan.bold("Type:")} ${chalk.white.bold(star.type)}`);
+    log(`${chalk.cyan.bold("Mass:")} ${chalk.white.bold(star.mass)}`);
+    log(`${chalk.cyan.bold("Diameter:")} ${chalk.white.bold(star.diameter)}`);
+    log(`${chalk.cyan.bold("Distance from earth:")} ${chalk.white.bold(star.distanceFromEarth)}`);
+    log(`${chalk.cyan.bold("Temperature:")} ${chalk.white.bold(star.temperature)}`);
+    log(`${chalk.cyan.bold("Composition:")} ${chalk.white.bold(star.composition)}`);
+    log(`${chalk.cyan.bold("Atmosphere:")} ${chalk.white.bold(star.atmosphere)}`);
+    log(`${chalk.cyan.bold("Fusion:")} ${chalk.white.bold(star.fusion)}`);
+    log(`${chalk.cyan.bold("planet:")} ${chalk.white.bold(star.planet)}`);
 };
 
 function exitOrContinueFunc() {
     const exitOrContinue = readlineSync.question(`${chalk.yellow.bold.underline("Do you want to know about our solar system [y/n]")}${chalk.yellow.bold(":")} `);
     if(exitOrContinue === "y") {
-        solarSystem();
+        solarSystem()
     } else if(exitOrContinue === "n") {
-        exit();
+        exit()
     } else {
         log(chalk.red.bold("Ooop! error occured, Please enter [y/n] to continue"));
-        exitOrContinueFunc();
+        exitOrContinueFunc()
     };
 };
 
